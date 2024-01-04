91a7e79457471d0fb90a0b7f56dc4e9f01e90271
Shugmi Shumunov <SShumunov@gmail.com>
2024-01-03 21:44:08 -0500

    refactor: `event.keyCode` is deprecated, use `event.key` instead

--- js/main.js
+++ js/main.js
@@ -324,32 +324,32 @@ const colors = [
 
 document.addEventListener('keydown', event => {
 	// left arrow
-	if (event.keyCode === 37){
+	if (event.key === 'ArrowLeft'){
 		event.preventDefault();
 		playerMove(-1);
 	}
 	// up arrow - rotate
-	if (event.keyCode === 38){
+	if (event.key === 'ArrowUp'){
 		event.preventDefault();
 		playerRotate(1);
 	}
 	// right arrow
-	if (event.keyCode === 39){
+	if (event.key === 'ArrowRight'){
 		event.preventDefault();
 		playerMove(1);
 	}
 	// down arrow - drop
-	if (event.keyCode === 40){
+	if (event.key === 'ArrowDown'){
 		event.preventDefault();
 		playerDrop();
 	}
 	// 'Q' - rotate
-	if (event.keyCode === 81){
+	if (event.key === 'q'){
 		event.preventDefault();
 		playerRotate(-1);
 	}
 	// 'W' - rotate
-	if (event.keyCode === 87){
+	if (event.key === 'w'){
 		event.preventDefault();
 		playerRotate(1);
 	}
